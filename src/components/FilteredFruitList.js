c2d43098bc522e2eb5ba2ce70b86e2fd51022fce
brookescarlett <brookeyalof2017@u.northwestern.edu>
2018-04-04 16:49:29 -0400

    Done.

--- src/components/FilteredFruitList.js
+++ src/components/FilteredFruitList.js
@@ -1,29 +1,53 @@
 import React, { Component } from 'react';
 
-class FilteredFruitList extends Component {
-  constructor(props) {
-    super(props);
+// class FilteredFruitList extends Component {
+//   constructor(props) {
+//     super(props);
+//
+//     this.state = {
+//       items: []
+//     };
+//   }
+//
+//   componentDidMount() {
+//     fetch('/api/fruit')
+//       .then(response => response.json())
+//       .then(items => this.setState({ items }));
+//   }
+//
+//   render() {
+//     const list = !this.props.filter || this.props.filter === 'all' ? this.state.items : this.state.items.filter(i => i.fruit_type === this.props.filter);
+//
+//     return (
+//       <ul className="fruit-list">
+//         {list.map((item, index) => <li key={index}>{item.char}</li>)}
+//       </ul>
+//     );
+//   }
+// }
 
-    this.state = {
-      items: []
-    };
-  }
 
-  componentDidMount() {
-    fetch('/api/fruit')
-      .then(response => response.json())
-      .then(items => this.setState({ items }));
-  }
 
-  render() {
-    const list = !this.props.filter || this.props.filter === 'all' ? this.state.items : this.state.items.filter(i => i.fruit_type === this.props.filter);
+const FilteredFruitList= (props) => {
 
-    return (
-      <ul className="fruit-list">
-        {list.map((item, index) => <li key={index}>{item.char}</li>)}
-      </ul>
-    );
-  }
+  FilteredFruitList.defaultProps = {
+    fruit: props.fruit,
+    filter: props.filter,
+    items: props.items
+  };
+
+  const list = !props.filter ? props.fruit : props.fruit.filter(item => item.fruit_type === props.filter)
+
+  console.log(props.fruit)
+  console.log(props.filter)
+  console.log(props.items)
+
+  return (
+    <ul className="fruit-list">
+      {list.map((item, index) => <li key={index}>{item.char}</li>)}
+    </ul>
+  );
+
 }
 
 export default FilteredFruitList;
